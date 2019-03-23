b52b18fac3c5213c7e69c22edde9bf4f8d8b3473
klaute <klaute@gmail.com>
2019-03-23 09:12:31 +0100

    Added -prj flag to print out the json representation of the yaml file.

--- Bastelbrothers/toolheim/src/main.go
+++ Bastelbrothers/toolheim/src/main.go
@@ -5,11 +5,13 @@ import (
 	"io/ioutil"
 
 	"toolheim"
-	//"fmt"
+	"fmt"
+	"github.com/ghodss/yaml"
 )
 
 var warbandFile = flag.String("warband", "", ".mordheim warband definition file to work with")
 var multiPage = flag.Bool("multipage", false, ".flag to generate multipage PDF")
+var prJSON = flag.Bool("prj", false, ".print json representation")
 
 func main() {
 	flag.Parse()
@@ -22,4 +24,12 @@ func main() {
 	warband := toolheim.ParseWarband(yamlContent)
 	//fmt.Println(warband.Rating)
 	toolheim.MakePDF(warband, *multiPage)
+
+	if *prJSON {
+		j, err := yaml.YAMLToJSON(yamlContent)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(j))
+	}
 }
