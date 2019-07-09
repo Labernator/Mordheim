9792f57705162943c48ddd92edaf883cf32d0dfc
klaute <klaute@gmail.com>
2019-07-09 10:07:38 +0200

    fixed player kai folder

---
+++ Mordheim-BorderTownBurning/Warband Rosters/Kai/gen_pdf.bat
@@ -0,0 +1,13 @@
+@echo off
+del warband-roaster.pdf
+set TMP_DIR=%cd%
+echo %TMP_DIR%
+set GOPATH=c:\users\klaute\Dropbox\Privat\Mordheim\Bastelbrothers\toolheim\
+c:
+cd c:\users\klaute\Dropbox\Privat\Mordheim\Bastelbrothers\toolheim\src\
+
+toolheim.exe -warband "%TMP_DIR%"\orks_n_goblins_n_squigs.mordheim.yml
+
+move warband-roaster.pdf "%TMP_DIR%"
+
+pause
